5877bc57129972083e44c9b15ecfd24b46ca49f7
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2021-02-02 14:32:22 +0300

    İlerleme sağlandı

---
+++ Core/Utilities/Result/IDataResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Result
+{
+    public interface IDataResult<out T>:IResult
+    {
+        T Data { get; }
+    }
+}
