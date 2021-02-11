b7857929a7d1ab31a1ccb3f1eaf894223596bc19
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2021-02-11 15:23:23 +0300

    ValidationTool okey

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static async Task ValidateAsync<T>(AbstractValidator<T> validator,T entity)
+        {
+            var result =await validator.ValidateAsync(entity);
+        }
+    }
+}
--- WebApi/Controllers/QueryController.cs
+++ WebApi/Controllers/QueryController.cs
@@ -2,6 +2,7 @@
 using Enities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,8 @@ namespace WebApi.Controllers
         [HttpPost]
         public async Task<IActionResult>AddProduct(Product product)
         {
+            var jsonData = JsonConvert.SerializeObject(product);
+           var classData =  JsonConvert.DeserializeObject<Product>(jsonData);
            var result =  await _metaDataService.AddProductAsync(product);
             return Ok(result.Message);
         }
